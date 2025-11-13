993645c35b39d507523b7e7537c9f867ce4ae7b3
Himanshu Dubey <101963392+himanshud2611@users.noreply.github.com>
2025-11-13 14:12:08 +0530

    Revert "Merge pull request #13 from himanshud2611/revert-12-dynamic-podcasts"
    This reverts commit ef9e0b940519c0e646ba4ee8c386ea06da086c81.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,13 @@ import mdx from '@astrojs/mdx';
 import icon from "astro-icon";
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import node from '@astrojs/node';
 
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [icon(), mdx({
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex]
